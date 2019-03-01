1eb56bd2873b4caa897575265b437feb6e6f2529
pink1999 <969909834@qq.com>
2019-03-01 09:05:20 +0800

    WPF主界面和登陆界面

---
+++ Login/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //关闭当前窗口
+            this.Hide();
+            //判断用户选择的是哪个窗口并显示该窗口
+            if (radio_sign.IsChecked==true)
+            {
+
+            }
+            if(radio_manage.IsChecked==true)
+            {   //打开管理系统窗口
+                string select = "manage";
+                PassWordWindow passWordWindow = new PassWordWindow(select);
+                passWordWindow.ShowDialog();
+                this.Hide();
+            }
+        }
+
+        private void Radio_sign_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Login/PassWordWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// PassWordWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PassWordWindow : Window
+    {
+        public string select { get; set; }
+        public PassWordWindow()
+        {
+            InitializeComponent();
+        }
+        public PassWordWindow(string s)
+        {
+            select = s;
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //若用户没有输入账号密码就提示用户
+            if (user.Text.Length==0 && password.Password.Length==0)
+            {
+                MessageBox.Show("请输入账号和密码！");
+            }
+            if (user.Text.Length==0 && password.Password.Length!=0)
+            {
+                MessageBox.Show("请输入账号！");
+            }
+            if (user.Text.Length!=0&&password.Password.Length==0)
+            {
+                MessageBox.Show("请输入密码！");
+            }
+            //判断账号密码是否正确
+            if (user.Text.ToString() == "yjy" && password.Password.ToString() == "00000")
+            {
+                //正确，登陆成功
+                if ("manage" == select)
+                {
+                    ManageSystem manageWindow = new ManageSystem();
+                    manageWindow.ShowDialog();
+                    this.Hide();
+                }
+                
+            }
+            else
+                MessageBox.Show("账号或密码有误！");
+        }
+    }
+}
